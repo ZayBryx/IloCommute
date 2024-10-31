6653495895d5796299e4ec3b7aed7fe784fd6a76
ZyBryx <zybryxm@gmail.com>
2024-10-31 22:30:23 +0800

    🎉 Initial commite

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,59 +1,36 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import { useEffect, useState, ReactNode } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Stack, useRouter } from "expo-router";
 
-import { useColorScheme } from '@/components/useColorScheme';
+interface LayoutProps {
+  children: ReactNode;
+}
 
-export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
-} from 'expo-router';
+const RootLayout = ({ children }: LayoutProps) => {
+  const [hasFirstLaunch, setHasFirstLaunch] = useState<boolean | null>(null);
+  const router = useRouter();
 
-export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
-};
+  const checkIfLaunch = async () => {
+    const isLaunch = await AsyncStorage.getItem("has_launched");
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
-export default function RootLayout() {
-  const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    ...FontAwesome.font,
-  });
-
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
-  useEffect(() => {
-    if (error) throw error;
-  }, [error]);
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
+    if (!isLaunch) {
+      setHasFirstLaunch(true);
+      await AsyncStorage.setItem("has_launched", "true");
+      router.replace("/(introduction)");
+    } else {
+      setHasFirstLaunch(false);
     }
-  }, [loaded]);
+  };
 
-  if (!loaded) {
+  useEffect(() => {
+    checkIfLaunch();
+  }, []);
+
+  if (hasFirstLaunch === null) {
     return null;
   }
 
-  return <RootLayoutNav />;
-}
+  return <Stack>{children}</Stack>;
+};
 
-function RootLayoutNav() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
-    </ThemeProvider>
-  );
-}
+export default RootLayout;
